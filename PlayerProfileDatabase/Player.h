ac08215d145067464453c441bc4be2bbdacdc2e0
Brionna Franklin <54289307+brionnafranklin@users.noreply.github.com>
2020-02-06 22:17:22 -0600

    Update Player.h

--- PlayerProfileDatabase/Player.h
+++ PlayerProfileDatabase/Player.h
@@ -4,17 +4,28 @@
 class Player
 {
 public:
+	//default constructer
 	Player();
+	//constructer; takes in name as a const char array and
 	Player(const char name[30], int score);
+	//default deconstructer
 	~Player();
+	//constructer; takes in a pointer to name and score as an int
 	Player(char* Name, int score);
+	
+	//sets the name of the player
 	void setName(const char name[30]);
+	//sets the score of the player
 	void setScore(const int score);
 	
+	//saves the player's name and score
 	void save(std::ofstream& out);
+	//loads the players name and score
 	bool load(std::ifstream& in);
 
+	//character array that hold the player's name
 	char m_name[30];
+	//intiger that holds the player's score
 	int m_score;
 };
 
