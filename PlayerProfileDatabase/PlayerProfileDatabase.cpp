d30d7f4690514adb3b5d6afe4c939d9f4fe4a918
Brionna Franklin <54289307+brionnafranklin@users.noreply.github.com>
2020-02-06 22:29:12 -0600

    Update PlayerProfileDatabase.cpp

--- PlayerProfileDatabase/PlayerProfileDatabase.cpp
+++ PlayerProfileDatabase/PlayerProfileDatabase.cpp
@@ -2,8 +2,11 @@
 #include <iostream>
 #include "DataBase.h"
 
+//runs when programs start
 int main()
 {
+	//creates an instinst of DataBase
 	DataBase dataBase;
+	//calls sart in DataBase
 	dataBase.start();
 }
